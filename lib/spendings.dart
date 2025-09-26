f0338b2233794404823643c3e54c6a6e7e36d613
Flugeeer <vkatashov16@gmail.com>
2025-09-27 01:19:07 +0300

    added a screen for a future realisation of additing spendings to your wallet

---
+++ lib/spendings.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:myfinancesapp/main.dart';
+
+class CreateSpendings extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Spendings'),
+        backgroundColor: Colors.lightGreen,
+      ),
+    );
+  }
+}
